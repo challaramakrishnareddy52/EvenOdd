bd3e71e972b602f6915a89635718325195fe00cc
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-11 20:01:22 +0530

    Even or Odd

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int Num = int.Parse(Console.ReadLine());
+            if (Num % 2 == 0)
+            {
+                Console.WriteLine(Num + " is a even number.");
+            }
+            else
+            {
+                Console.WriteLine(Num + " is a odd number.");
+            }
+        }
+    }
+}
